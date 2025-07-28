5c054728d0599a3be22ebe5090dc63cd5e9019d9
Zoly24 <alexzanderwilburn2424@outlook.com>
2025-07-28 11:45:05 -0500

    New hot pink rice

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,20 @@ return {
     "neanias/everforest-nvim",
     background = "hard",
     config = function()
-      vim.o.background = dark, vim.cmd("colorscheme everforest")
+      --vim.o.background = dark, vim.cmd("colorscheme everforest")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      --vim.cmd("colorscheme rose-pine-moon")
+    end,
+  },
+  {
+    "Shadorain/shadotheme",
+    config = function()
+      vim.cmd("colorscheme shado-legacy")
     end,
   },
 }
