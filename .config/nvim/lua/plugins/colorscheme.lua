1b4a9c147a9f567d8100cf3473601ff3a8242b21
Zoly24 <alexzanderwilburn2424@outlook.com>
2025-08-16 19:20:51 -0500

    Adjusted waybar and added fontconfig

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -16,7 +16,68 @@ return {
   {
     "Shadorain/shadotheme",
     config = function()
-      vim.cmd("colorscheme shado-legacy")
+      --vim.cmd("colorscheme shado-legacy")
+    end,
+  },
+  {
+    "atelierbram/Base2Tone-nvim",
+    config = function()
+      -- dark schemes
+      -- vim.cmd("colorscheme base2tone_evening_dark")
+      -- vim.cmd("colorscheme base2tone_morning_dark")
+      -- vim.cmd("colorscheme base2tone_sea_dark")
+      -- vim.cmd("colorscheme base2tone_space_dark")
+      -- vim.cmd("colorscheme base2tone_earth_dark")
+      -- vim.cmd("colorscheme base2tone_forest_dark")
+      -- vim.cmd("colorscheme base2tone_field_dark")
+      -- vim.cmd("colorscheme base2tone_garden_dark")
+      -- vim.cmd("colorscheme base2tone_desert_dark")
+      -- vim.cmd("colorscheme base2tone_lake_dark")
+      -- vim.cmd("colorscheme base2tone_meadow_dark")
+      -- vim.cmd("colorscheme base2tone_drawbridge_dark")
+      -- vim.cmd("colorscheme base2tone_mall_dark")
+      -- vim.cmd("colorscheme base2tone_suburb_dark")
+      -- vim.cmd("colorscheme base2tone_lavender_dark")
+      -- vim.cmd("colorscheme base2tone_pool_dark")
+      -- vim.cmd("colorscheme base2tone_porch_dark")
+      -- vim.cmd("colorscheme base2tone_heath_dark")
+      -- vim.cmd("colorscheme base2tone_cave_dark")
+      -- vim.cmd("colorscheme base2tone_motel_dark")
+
+      -- light schemes
+      -- vim.cmd("colorscheme base2tone_evening_light")
+      -- vim.cmd("colorscheme base2tone_morning_light")
+      -- vim.cmd("colorscheme base2tone_sea_light")
+      -- vim.cmd("colorscheme base2tone_space_light")
+      -- vim.cmd("colorscheme base2tone_earth_light")
+      -- vim.cmd("colorscheme base2tone_forest_light")
+      -- vim.cmd("colorscheme base2tone_field_light")
+      -- vim.cmd("colorscheme base2tone_garden_light")
+      -- vim.cmd("colorscheme base2tone_desert_light")
+      -- vim.cmd("colorscheme base2tone_lake_light")
+      -- vim.cmd("colorscheme base2tone_meadow_light")
+      -- vim.cmd("colorscheme base2tone_drawbridge_light")
+      -- vim.cmd("colorscheme base2tone_mall_light")
+      -- vim.cmd("colorscheme base2tone_suburb_light")
+      -- vim.cmd("colorscheme base2tone_lavender_light")
+      -- vim.cmd("colorscheme base2tone_pool_light")
+      -- vim.cmd("colorscheme base2tone_porch_light")
+      -- vim.cmd("colorscheme base2tone_heath_light")
+      -- vim.cmd("colorscheme base2tone_cave_light")
+      -- vim.cmd("colorscheme base2tone_motel_light")
+    end,
+  },
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    config = function()
+      vim.cmd("colorscheme oxocarbon")
+      vim.opt.background = "dark"
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    config = function()
+      --vim.cmd("colorscheme catppuccin-frappe")
     end,
   },
 }
