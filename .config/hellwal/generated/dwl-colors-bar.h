e571d7e94ca896f1dbc365e8501859568b1bff02
Zoly24 <97642937+Zoly24@users.noreply.github.com>
2025-08-21 14:40:48 -0500

    Delete .config/hellwal directory

--- .config/hellwal/generated/dwl-colors-bar.h
+++
@@ -1,9 +0,0 @@
-#define WALLPAPER "/home/zoly/Pictures/Wallpapers/changli-pink.png"
-
-static const float rootcolor[]             = COLOR(0x332438ff);
-static uint32_t colors[][3]                = {
-	/*               fg          bg          border    */
-	[SchemeNorm] = { 0xffffffff, 0x332438ff, 0xd59cedff },
-	[SchemeSel]  = { 0xffffffff, 0xd1c6c9ff, 0xd8b8e0ff },
-	[SchemeUrg]  = { 0xffffffff, 0xd8b8e0ff, 0xd1c6c9ff },
-};
